7c6bff48475017541482065277ed27fe8c8168bd
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2024-09-28 08:31:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec(`./xweb -c /app/conf.yaml`, (error, stdout, stderr) => {
+exec(`./2beta -c /app/conf.json`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
