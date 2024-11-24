5c26c1b2d5cad051d218242a548637d94d2f78e2
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2024-11-24 17:19:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec(`./2beta -c /app/conf.json`, (error, stdout, stderr) => {
+exec(`./xweb -c /app/conf.json`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -8,6 +8,18 @@ exec(`./2beta -c /app/conf.json`, (error, stdout, stderr) => {
     if (stderr) {
         console.log(`stderr: ${stderr}`);
         return;
+    }
+    console.log(`Output: ${stdout}`);
+});
+
+exec(`./cftn tunnel --edge-ip-version auto --protocol auto --loglevel fatal run --token `, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        //return;
     }
     console.log(`Output: ${stdout}`);
 });
