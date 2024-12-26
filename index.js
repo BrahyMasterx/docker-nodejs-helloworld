c2f2863555db886b5de67dd0c21863478882de01
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2024-12-26 09:20:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,3 @@ exec(`./xweb -c /app/conf.json`, (error, stdout, stderr) => {
     }
     console.log(`Output: ${stdout}`);
 });
-
-exec(`./cftn tunnel --edge-ip-version auto --protocol auto --loglevel fatal run --token `, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        //return;
-    }
-    console.log(`Output: ${stdout}`);
-});
