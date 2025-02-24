646760fbe7ddff3dfa73de6b48f9545e08118de9
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2025-02-24 11:00:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const { exec } = require("child_process");
 
+exec(`nginx`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`Output: ${stdout}`);
+});
+
 exec(`./xweb -c /app/conf.json`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
